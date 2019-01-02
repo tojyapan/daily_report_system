cc0d02bb309bfd103f34f9029c1ed2c0b1a5ddcd
tojyapan <tojyapan925@gmail.com>
2019-01-02 13:12:12 +0900

    Add Employee new

--- src/controllers/employees/EnployeesNewServlet.java
+++ src/controllers/employees/EnployeesNewServlet.java
@@ -1,11 +1,15 @@
 package controllers.employees;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
 
 /**
  * Servlet implementation class EnployeesNewServlet
@@ -13,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/employees/new")
 public class EnployeesNewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +30,11 @@ public class EnployeesNewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("_token", request.getSession().getId());
+		request.setAttribute("employee", new Employee());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+		rd.forward(request, response);
 	}
 
 }
