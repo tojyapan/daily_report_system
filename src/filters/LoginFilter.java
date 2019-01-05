1218d32a570d4dcc31e1f978571d6ff71bcb94d1
tojyapan <tojyapan925@gmail.com>
2019-01-05 15:56:38 +0900

    Add login / layout link

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -1,6 +1,7 @@
 package filters;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,6 +9,11 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.Employee;
 
 /**
  * Servlet Filter implementation class LoginFilter
@@ -16,7 +22,7 @@ import javax.servlet.annotation.WebFilter;
 public class LoginFilter implements Filter {
 
     /**
-     * Default constructor. 
+     * Default constructor.
      */
     public LoginFilter() {
         // TODO Auto-generated constructor stub
@@ -33,10 +39,31 @@ public class LoginFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+		String context_path = ((HttpServletRequest)request).getContextPath();
+		String servlet_path = ((HttpServletRequest)request).getServletPath();
 
-		// pass the request along the filter chain
+		if (!servlet_path.matches("/css.*")) {
+			HttpSession session = ((HttpServletRequest)request).getSession();
+			Employee e = (Employee)session.getAttribute("login_employee");
+
+			if (!servlet_path.equals("/login")) {
+				if (e == null) {
+					((HttpServletResponse)response).sendRedirect(context_path + "/login");
+					return;
+				}
+
+				if (servlet_path.matches("/employees.*") && e.getAdmin_flag() == 0) {
+					((HttpServletResponse)response).sendRedirect(context_path + "/");
+					return;
+				}
+			} else {
+				if (e != null) {
+					((HttpServletResponse)response).sendRedirect(context_path + "/");
+					return;
+				}
+			}
+		}
+
 		chain.doFilter(request, response);
 	}
 
